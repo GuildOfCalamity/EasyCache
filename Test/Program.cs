6f0256167714273154f814bd7e4691920e6a1110
GuildOfCalamity <81915293+GuildOfCalamity@users.noreply.github.com>
2024-10-14 16:27:28 -0400

    test updates

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,4 +1,5 @@
-﻿using EasyCache;
+﻿using System;
+using EasyCache;
 
 namespace Test;
 
@@ -9,50 +10,89 @@ internal class Program
         Console.OutputEncoding = System.Text.Encoding.UTF8;
         AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
 
-        #region [Using my home-brew cache]
-        Console.WriteLine("\r\n  Home-Brew Cache Test  \r\n↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓\r\n");
-        var cache = new CacheHelper<string>();
-        cache.ItemEvicted += OnItemEvicted;
-        cache.ItemUpdated += OnItemUpdated;
-        try
-        {
-            cache.AddOrUpdate("key1", GenerateKeyValue(), TimeSpan.FromSeconds(3));
-            cache.AddOrUpdate("key2", GenerateKeyValue(), TimeSpan.FromHours(24));
-            cache.AddOrUpdate("key3", GenerateKeyValue(), TimeSpan.FromDays(5));
-            cache.AddOrUpdate("key5", GenerateKeyValue(), DateTime.Now.AddMinutes(1));
-            var keys = cache.GetAllKeys();
-            Console.WriteLine($"Current cache keys: {string.Join(", ", keys)}");
-            Thread.Sleep(6000);
-            cache.AddOrUpdate("key5", GenerateKeyValue(), DateTime.Now.AddMinutes(2));
-            var dt = cache.GetExpiration("key5");
-            if (dt != null)
-                Console.WriteLine($"\"key5\" will expire at {dt.Value.ToLongTimeString()} on {dt.Value.ToLongDateString()}");
-            Console.WriteLine($"The current cache does {(cache.Contains("key1") ? "" : "not")} contain the key \"key1\"");
-            Console.WriteLine($"The current cache does {(cache.Contains("key2") ? "" : "not")} contain the key \"key2\"");
-            cache.Remove("key2");
-            foreach (var ci in cache.GetCacheAsEnumerable())
-            {
-                Console.WriteLine($"Cache item {ci.Key} value: {ci.Value.Value}");
-            }
-        }
-        finally 
-        { 
-            cache.Dispose();
-            Thread.Sleep(3000);
-        }
-        #endregion
+        Console.WriteLine("\r\n Press [1] for home-brew cache, [2] for System.Runtime.Caching, any other key to exit. ");
 
+        var selection = Console.ReadKey(true).Key;
+        if (selection == ConsoleKey.D1)
+        {
+            RunHomeBrewTest();
+            _ = Console.ReadKey(true);
+        }
+        else if (selection == ConsoleKey.D2)
+        {
+            RunMSLibTest();
+        }
+        else
+        {
+            Environment.Exit(0);
+        }
+    }
+
+    static void RunHomeBrewTest()
+    {
+        _ =Task.Run(async () =>
+        {
+            #region [Using my home-brew cache]
+            Console.WriteLine("\r\n  Home-Brew Cache Test  \r\n↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓\r\n");
+            var cache = new CacheHelper<string>();
+            cache.ItemEvicted += OnItemEvicted;
+            cache.ItemUpdated += OnItemUpdated;
+            try
+            {
+                cache.AddOrUpdate("key1", GenerateKeyValue(), TimeSpan.FromSeconds(3));
+                cache.AddOrUpdate("key2", GenerateKeyValue(), TimeSpan.FromHours(24));
+                cache.AddOrUpdate("key3", GenerateKeyValue(), TimeSpan.FromDays(30));
+                cache.AddOrUpdate("key4", GenerateKeyValue(), DateTime.Now.AddMinutes(1));
+                cache.AddOrUpdate("key5", GenerateKeyValue(), TimeSpan.FromSeconds(7));
+                var keys = cache.GetAllKeys();
+                Console.WriteLine($"Current cache keys: {string.Join(", ", keys)}");
+                await Task.Delay(6000);
+
+                var key5 = cache.Get("key5"); // refresh the expire by fetching
+                if (string.IsNullOrEmpty(cache.Get("unknown")))
+                    Console.WriteLine($"Key \"unknown\" does not exist.");
+
+                var dt = cache.GetExpiration("key5");
+                if (dt != null)
+                    Console.WriteLine($"\r\n\"key5\" will expire at {dt.Value.ToLongTimeString()} on {dt.Value.ToLongDateString()}");
+
+                await Task.Delay(6000);
+
+                Console.WriteLine($"The current cache does {(cache.Contains("key1") ? "" : "not ")}contain the key \"key1\"");
+                Console.WriteLine($"The current cache does {(cache.Contains("key2") ? "" : "not ")}contain the key \"key2\"");
+                Console.WriteLine($"The current cache does {(cache.Contains("key3") ? "" : "not ")}contain the key \"key3\"");
+                Console.WriteLine($"The current cache does {(cache.Contains("key5") ? "" : "not ")}contain the key \"key5\"");
+                Console.WriteLine();
+
+                cache.Remove("key2");
+
+                foreach (var ci in cache.GetCacheAsEnumerable())
+                {
+                    Console.WriteLine($"Cache item {ci.Key} value: {ci.Value.Value}");
+                }
+            }
+            finally
+            {
+                await Task.Delay(4000);
+                cache.Dispose();
+            }
+            #endregion
+        });
+    }
+
+    static void RunMSLibTest()
+    {
         #region [Using Microsoft's System.Runtime.Caching]
         int minSeconds = 10;
         string keyName = "password";
         Console.WriteLine("\r\n  System.Runtime.Caching Test  \r\n↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓\r\n");
-        CacheHelper.OnCacheItemUpdated += (k, r) => 
-        { 
-            Console.WriteLine($"[CacheUpdate] ⇒ [{k}][{r}]"); 
+        CacheHelper.OnCacheItemUpdated += (k, r) =>
+        {
+            Console.WriteLine($"[CacheUpdate] ⇒ [{k}][{r}]");
         };
-        CacheHelper.OnCacheException += (e) => 
-        { 
-            Console.WriteLine($"[CacheError] ⇒ [{e}]"); 
+        CacheHelper.OnCacheException += (e) =>
+        {
+            Console.WriteLine($"[CacheError] ⇒ [{e}]");
         };
 
         Console.WriteLine("• Press any key to access the cache or [Esc] to exit.\r\n");
@@ -76,9 +116,11 @@ internal class Program
         {
             switch (_key)
             {
-                case ConsoleKey.C: Console.Clear();
+                case ConsoleKey.C:
+                    Console.Clear();
                     break;
-                case ConsoleKey.D: Console.WriteLine($"[CONTENTS]");
+                case ConsoleKey.D:
+                    Console.WriteLine($"[CONTENTS]");
                     foreach (var item in CacheHelper.GetCacheItems())
                     {
                         if (item.Value is ExampleItem ci)
@@ -87,17 +129,20 @@ internal class Program
                             Console.WriteLine($"■ Key '{item.Key}' with value '{item.Value}'. ");
                     }
                     break;
-                case ConsoleKey.A: case ConsoleKey.U:
+                case ConsoleKey.A:
+                case ConsoleKey.U:
                     CacheHelper.AddOrUpdate(keyName, GenerateKeyValue(), TimeSpan.FromSeconds(10));
                     break;
-                case ConsoleKey.F: case ConsoleKey.G:
+                case ConsoleKey.F:
+                case ConsoleKey.G:
                     var fetch = CacheHelper.Get(keyName);
                     if (fetch != null)
                         Console.WriteLine($"■ Item retrieved from cache with key '{keyName}' of type '{fetch.GetType()}'");
                     else
                         Console.WriteLine($"■ Item not found in cache with key '{keyName}'");
                     break;
-                case ConsoleKey.R: CacheHelper.Remove(keyName);
+                case ConsoleKey.R:
+                    CacheHelper.Remove(keyName);
                     break;
             }
         }
@@ -129,17 +174,18 @@ internal class Program
     /// </para><para>
     ///   The base of possible codes is large (about 3.2 * 10^34).
     /// </para></summary>
-    static string GenerateKeyValue(int pLength = 6, long pSeed = 0)
+    static string GenerateKeyValue(int pLength = 8, long pSeed = 0)
     {
         const string pwChars = "2346789BCDFGHJKMPQRTVWXY";
-        if (pLength < 6)
-            pLength = 6; // minimum of 6 characters
+        if (pLength < 1)
+            pLength = 1;
 
         char[] charArray = pwChars.Distinct().ToArray();
 
         if (pSeed == 0)
             pSeed = DateTime.Now.Ticks;
 
+        Thread.Sleep(1); // force one tick for RNG
         var result = new char[pLength];
         var rng = new Random((int)pSeed);
 
@@ -149,10 +195,7 @@ internal class Program
         return (new string(result));
     }
 
-    static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
-    {
-        Console.WriteLine($"[ERROR] UnhandledException: {(e.ExceptionObject as Exception)?.Message}");
-    }
+    static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e) => Console.WriteLine($"[ERROR] UnhandledException: {(e.ExceptionObject as Exception)?.Message}");
 }
 
 /// <summary>
